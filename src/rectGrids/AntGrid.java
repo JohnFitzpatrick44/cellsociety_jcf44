3e42ecebf05ef2963d60f7b7184d74fcf59fe5bf
Jack Fitzpatrick <jcf44@duke.edu>
2018-02-12 01:10:19 -0500

    Added ant agents to grid

--- src/rectGrids/AntGrid.java
+++ src/rectGrids/AntGrid.java
@@ -1,5 +1,7 @@
 package rectGrids;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ThreadLocalRandom;
 
 import XML.AntHolder;
@@ -19,6 +21,7 @@ public class AntGrid extends RectangleGrid {
 		Cell[][] grid = new Cell[gridSize][gridSize];
 		int heightSpacing = 0;
 		int index = 0;
+		ArrayList<double[]> nestCells = new ArrayList<>();
 		for(int i=0;i<grid.length;i++) {
 			int blockSpacing = 0;
 			for(int j=0;j<grid[i].length;j++) {
@@ -27,8 +30,11 @@ public class AntGrid extends RectangleGrid {
 							  offset+blockSpacing+cellWidth,offset+heightSpacing,
 							  offset+blockSpacing+cellWidth, offset+heightSpacing+cellHeight,
 							  offset+blockSpacing,offset+heightSpacing+cellHeight);
+					if(gridConfig[index] == 2) {
+						nestCells.add(new int[] {i,j});
+					}
 				} else {
-					grid[i][j] = new AntCell(5, offset+blockSpacing,offset+heightSpacing,
+					grid[i][j] = new AntCell(0, offset+blockSpacing,offset+heightSpacing,
 							  offset+blockSpacing+cellWidth,offset+heightSpacing,
 							  offset+blockSpacing+cellWidth, offset+heightSpacing+cellHeight,
 							  offset+blockSpacing,offset+heightSpacing+cellHeight);
@@ -40,13 +46,8 @@ public class AntGrid extends RectangleGrid {
 			heightSpacing += cellHeight;
 		}
 		for(int k = 0; k < NUM_AGENTS; k++) {
-			int i = ThreadLocalRandom.current().nextInt(0, gridSize);
-			int j = ThreadLocalRandom.current().nextInt(0, gridSize);
-			
-			while(!((AntCell) grid[i][j]).roomForAnts()) {
-				i = ThreadLocalRandom.current().nextInt(0, gridSize);
-				j = ThreadLocalRandom.current().nextInt(0, gridSize);
-			}
+			Collections.shuffle(nestCells);
+			while(!((AntCell) grid[nestCells.get(0)[0]][nestCells.get(0)[1]]).roomForAnts())
 			
 			AntAgent sa = new AntAgent((AntCell) grid[i][j]);
 			((AntCell) grid[i][j]).addAnt(sa);
